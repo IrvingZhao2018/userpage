17bf1e221f8fcf7b52bf5ba4ba34a8d1560c97a6
irving <irvingzqy@gmail.com>
2020-09-19 01:36:14 -0400

    optimize import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import UserInput from "./UserInput/UserInput";
 import UserOutput from "./UserOutput/UserOutput";
